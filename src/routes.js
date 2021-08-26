65022aec8d3ac1c35067da15644843ba31339f0c
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-25 21:14:38 -0300

    componente app

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Security from './core/components/security/Security';
 
 
 export default () => {
     return (
         <BrowserRouter>
         <Switch>
-            
+            <Route path="/" exact component={Security}></Route>
         </Switch>
         </BrowserRouter>
     )
