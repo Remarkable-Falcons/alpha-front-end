d4dc4bdf08448b8fb5a90e107198d426f3cbaaee
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-25 21:11:36 -0300

    index e app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 //integrar com o bootstrap:
 //import 'bootstrap/dist/css/bootstrap.min.css';
-
-import App from './App'
 ReactDOM.render(
     <App></App>,
     document.getElementById('root')
